a1874ef0a72f0893c14394afa501b686f521c8ca
victorhugofg <69266153+victorhugofg@users.noreply.github.com>
2023-09-15 16:54:29 +0200

    Update driver training GPRO.py
    V2 with full training module

--- driver training GPRO.py
+++ driver training GPRO.py
@@ -26,9 +26,59 @@ driver_aspect = driver_data.split(',')
 # Create a DataFrame with the user input
 user_df = pd.DataFrame([driver_aspect], columns=df.columns[1:])  # Exclude the 'Training' column
 
-#Concentration
-if int(driver_aspect[0]) < 219:
-     print("Yoga")
+# Define the aspect ranking (higher values are better)
+aspect_ranking = {
+    'Concentrat.': 0,
+    'Stamina': 1,
+    'Weight': 2,
+    'Technical': 3,
+    'Aggres.': 4,
+    'Motivat.': 5,
+    'Charisma': 6,
+}
 
-if int(driver_aspect[0]) > 219:
-    print("CON too high")
+# Sort the aspects by ranking (ascending order)
+sorted_aspects = sorted(aspect_ranking.keys(), key=lambda x: aspect_ranking[x])
+
+# Track when each aspect reaches its target
+concentration_met = False
+stamina_met = False
+weight_met = False
+technical_met = False
+aggression_met = False
+motivation_met = False
+
+# Training
+for aspect in sorted_aspects:
+    value = int(driver_aspect[aspect_ranking[aspect]])
+    if aspect == 'Concentrat.':
+        if value >= 219:
+            concentration_met = True
+        else:
+            print("Yoga")
+    elif aspect == 'Stamina':
+            if value >= 109:
+                stamina_met = True
+            elif stamina_met:
+                print("Fitness")
+            elif aspect == 'Weight':
+                if value < 81:
+                    weight_met = True
+                elif weight_met:
+                    print("Fitness")
+                elif aspect == 'Technical':
+                    if value > 149:
+                        technical_met = True
+                elif technical_met:
+                    print("Technical")
+                elif aspect == 'Aggres.':
+                        if value >= 49:
+                            aggression_met = True
+                        elif aggression_met:  # Corrected variable name here
+                            print("Ninja")
+                        elif aspect == 'Motivat.':
+                            motivation_met = True
+
+# If all high enough then maximize top 3
+if concentration_met and stamina_met and weight_met and technical_met and aggression_met and motivation_met:
+    print("Yoga")
